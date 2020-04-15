961cc54c2958703ca34343058f88f0a5c4ce8af2
armando555 <armandosebas4@gmail.com>
2020-04-15 15:06:55 -0500

    Ya funciona con lo tuyo

--- CalculateSubMatrix.py
+++ CalculateSubMatrix.py
@@ -1,8 +1,12 @@
 #This function calculate the sum a matrix from (0,0) to bottom right of a matrix
-def sumCompleteMatrix(mat):
+def sumCompleteMatrix(mat,point1,point2):
     sum=0
-    for i in range(0,len(mat)):
-        for j in range (0,len(mat[i])):
+    if point1==len(mat):
+        point1++
+    for i in range(0,point1):
+        if point2==len(mat[i]) and i==0:
+            point2++
+        for j in range (0,len(point2):
             sum+=mat[i][j]
     return sum
 #This function calculate the sum from i to j
@@ -12,5 +16,11 @@ def sumItoJ(i,j):
         neto+=m
     return neto
 
+def generarMatriz():
+	colum = input("Enter the colum size")
 
-print(sumCompleteMatrix([[1,2,3],[4,5,6]])==sumItoJ(1,6))
+
+if __name__=="__main__":
+	generarMatriz()
+
+print(sumCompleteMatrix([[1,2,3],[4,5,6],[7,8,9]])==sumItoJ(1,6))
