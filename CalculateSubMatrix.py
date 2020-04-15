0a85a779f8d4549754157e2f0e543d29fdc51faa
jdmejiav <jdmejiav@eafit.edu.co>
2020-04-15 14:57:47 -0500

    jeje

--- CalculateSubMatrix.py
+++ CalculateSubMatrix.py
@@ -1,6 +1,9 @@
 #This function calculate the sum a matrix from (0,0) to bottom right of a matrix
 
 
+
+
+
 def sumCompleteMatrix(mat):
     sum=0
     for i in range(0,len(mat)):
@@ -15,6 +18,14 @@ def sumItoJ(i,j):
     for m in range(i,j+1):
         neto+=m
     return neto
+
+
+def generarMatriz():
+	colum = input("Enter the colum size")
+	
+
+if __name__=="__main__":
+	generarMatriz()
 
 
 print(sumCompleteMatrix([[1,2,3],[4,5,6]])==sumItoJ(1,6))
